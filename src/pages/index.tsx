58a8bed1e5167fa093c4a0ab3dc14528f4d7f8d1
Sonseongoh <nobleser@naver.com>
2024-10-04 14:53:59 +0900

    index 페이지 ssg 방식 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
 // CSS Module
-import SearchableLayout from "@/components/searchable-layout";
-import style from "./index.module.css";
-import { ReactNode, useEffect } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import SearchableLayout from "@/components/searchable-layout";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-randomBooks";
+import { InferGetStaticPropsType } from "next";
+import { ReactNode } from "react";
+import style from "./index.module.css";
 
-export const getServerSideProps = async () => {
-  //컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
 
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
@@ -27,9 +26,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(allBooks);
-
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
