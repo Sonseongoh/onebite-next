071e8d4ae17634321d4423e3f74c0af5488a0c78
Sonseongoh <nobleser@naver.com>
2024-10-04 15:06:49 +0900

    쿼리스트링 사용으로 인해 빌드 타임에 데이터를 미리 불러올 수 없는 페이지에서 클라이언트 사이드 측에서 직접 데이터 패칭해서 불러오도록 변경(검색)

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,23 +1,40 @@
 import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
 import fetchBooks from "@/lib/fetch-books";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-import { ReactNode } from "react";
+import { IBookData } from "@/types";
+import {
+  GetServerSidePropsContext,
+  GetStaticPropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect, useState } from "react";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props: { books },
+//   return {
+//     props: { books },
+//   };
+// };
+
+export default function Page() {
+  const [books, setBooks] = useState<IBookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
 
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
+
   return (
     <div>
       {books.map((book) => (
